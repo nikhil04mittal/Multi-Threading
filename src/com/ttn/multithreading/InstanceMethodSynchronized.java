886e8708507c23a009aaa66e444fe87ce2b874aa
Nikhil <nikhil.mittal@tothenew.com>
2021-09-14 14:11:02 +0530

    Method sync

--- src/com/ttn/multithreading/InstanceMethodSynchronized.java
+++ src/com/ttn/multithreading/InstanceMethodSynchronized.java
@@ -3,43 +3,21 @@ package com.ttn.multithreading;
 public class InstanceMethodSynchronized {
     private Integer integer = 10;
     private synchronized void increment() {
-        System.out.println(Thread.currentThread().getName() + " is locking.");
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         this.integer++;
-        System.out.println(Thread.currentThread().getName() + " is unlocking.");
     }
     private synchronized void decrement() {
-        System.out.println(Thread.currentThread().getName() + " is locking.");
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         this.integer--;
-        System.out.println(Thread.currentThread().getName() + " is unlocking.");
-    }
-
-    private void print() {
-        System.out.println(Thread.currentThread().getName() + " is printing.");
-        System.out.println(this.integer);
-        System.out.println(Thread.currentThread().getName() + " is printed.");
     }
 
     public static void main(String[] args) throws InterruptedException {
         InstanceMethodSynchronized instanceMethodSynchronized = new InstanceMethodSynchronized();
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 20000; i++) {
-                instanceMethodSynchronized.print();
                 instanceMethodSynchronized.increment();
             }
         }, "IncrementThread");
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 20000; i++) {
-                instanceMethodSynchronized.print();
                 instanceMethodSynchronized.decrement();
             }
         }, "DecrementThread");
